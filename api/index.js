540bb5e7655d9beafe3edd907f5faa39c1d7147e
absol <rizwan.amir121@gmail.com>
2023-12-16 03:14:42 +0500

    vercel.json added

--- api/index.js
+++ api/index.js
@@ -22,6 +22,10 @@ mongoose
     console.log(err);
   });
 
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+})
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
