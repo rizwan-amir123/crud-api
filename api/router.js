e8adbe7430916338c321b033f820a89d00dec5bb
absol <rizwan.amir121@gmail.com>
2023-12-16 15:50:41 +0500

    new

--- api/router.js
+++ api/router.js
@@ -1,4 +1,6 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
+//const router = require("express").Router();
 const { getTasks, createTask, updateTask, deleteTask,
 getSingleTask, deleteAllTasks, findDoneTasks,
 findPendingTasks, findLateTasks, findProcessingTasks,
--- index.js
+++ index.js
@@ -20,14 +20,14 @@ mongoose.connect(process.env.MONGODB_URL, {
     console.log(err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
-})
+//app.get('/', (req, res) => {
+//    res.send('Hey this is my API running 🥳')
+//})
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(router);
+//app.use(router);
 app.listen(PORT, async () => {
   console.log(`server up on port ${PORT}`);
 });
