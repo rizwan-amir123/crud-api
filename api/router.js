2085f9c317955a03023702ce44ae1836c72d8856
absol <rizwan.amir121@gmail.com>
2023-12-16 17:15:00 +0500

    routes

--- api/router.js
+++ api/router.js
@@ -5,21 +5,21 @@ const { getTasks, createTask, updateTask, deleteTask,
 getSingleTask, deleteAllTasks, findDoneTasks,
 findPendingTasks, findLateTasks, findProcessingTasks,
 findNotAssignedTasks, patchTask} = require("../controllers/task");
-/*
+
+router.patch("/task/:taskID", patchTask);
+router.delete("/task", deleteAllTasks);
+router.delete("/task/:taskID", deleteTask);
+router.put("/task/:taskID", updateTask);
+router.post("/task", createTask);
+
+router.get("/task/:taskID", getSingleTask);
+router.get("/task", getTasks);
+router.get("/task/done", findDoneTasks);
 router.get("/task/pending", findPendingTasks);
 router.get("/task/late", findLateTasks);
 router.get("/task/processing", findProcessingTasks);
 router.get("/task/notassigned", findNotAssignedTasks);
 
-router.patch("/task/:taskID", patchTask);
-router.get("/task/done", findDoneTasks);
-router.delete("/task", deleteAllTasks);
-router.get("/task/:taskID", getSingleTask);
-router.delete("/task/:taskID", deleteTask);
-router.put("/task/:taskID", updateTask);
-router.post("/task", createTask);
-router.get("/task", getTasks);
-*/
 router.get("/", (req, res) => {
   res.send("Let's build a CRUD API!");
 });
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const router = require("./api/router");
+//const router = require("./api/router");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = 8000;
+const PORT = 3000;
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URL, {
@@ -23,11 +23,21 @@ mongoose.connect(process.env.MONGODB_URL, {
 app.get('/', (req, res) => {
     res.send('Hey this is my API running 🥳')
 })
-
+const res = require('./api/router.js');
+//const res = require('./api/router.js');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(router);
+//app.use(router);
+app.use('/api/', res);
+app.use('/api/task', res);
+app.use('/api/task/pending', res);
+app.use('/api/task/late', res);
+app.use('/api/task/processing', res);
+app.use('/api/task/notassigned', res);
+app.use('/api/task/done', res);
+app.use('/api/task/:taskID', res);
+
 app.listen(PORT, async () => {
   console.log(`server up on port ${PORT}`);
 });
